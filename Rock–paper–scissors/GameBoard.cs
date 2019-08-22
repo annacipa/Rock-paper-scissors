3c6f61c7f8d78282ed90935a085a32dd49c1e6c5
Dorontina Cipa <dorontina.cipa@nais.onmicrosoft.com>
2019-08-22 11:32:40 +0200

    fixed repeat round if both random choice is Rocks

--- Rock–paper–scissors/GameBoard.cs
+++ Rock–paper–scissors/GameBoard.cs
@@ -7,11 +7,17 @@ using System.IO;
 
 namespace Rock_paper_scissors
 {
-  public  class GameBoard
+    public class GameBoard
     {
-        private const int Rounds = 100;
-        private const String RockOfPC = "Rock";
-        private string[] Values = { "Rock", "Scissors", "Paper" };
+        private
+        const int Rounds = 100;
+        private
+        const String RockOfPC = "Rock";
+        private string[] Values = {
+   "Rock",
+   "Scissors",
+   "Paper"
+  };
         public List<Results> ResultArray = new List<Results>();
 
 
@@ -33,30 +39,40 @@ namespace Rock_paper_scissors
                 var index = random_value_of_opponent.Next(0, 3);
                 if (index == 2)
                 {
+                    //check the round
+                    Console.WriteLine("Round is {0}", i);
                     //INSERT TO ARRAY RESULTS OF USER-WINNER
                     ResultArray.Add(new Results(Values[index], i + 1, "user"));
-                    Console.Write("Wins user"+ Environment.NewLine);
+                    Console.Write("Wins user" + Environment.NewLine);
 
-                   
+
                 }
                 else if (index == 1)
                 {
+                    //check the round
+                    Console.WriteLine("Round is {0}", i);
                     //INSERT TO ARRAY RESULTS OF PC-WINNER 
                     ResultArray.Add(new Results(Values[index], i + 1, "pc"));
-                    Console.Write("Wins pc"+ Environment.NewLine);
+                    Console.Write("Wins pc" + Environment.NewLine);
                 }
                 else
                 {
-                   //NOW IT SKIPS, BUT IT SHOULD MAKE IT REPEAT
-                    Console.Write("Repeat"+ Environment.NewLine);
-                }
+                    //You output its a repeat and we need to redo the round
+
+
+                    Console.Write("Repeat | Chose Rock !|  Round is {0}", i);
+                    Console.WriteLine(Environment.NewLine);
+                    //increase i to go previous round when redo round
+                    i--;
 
                 }
-                 //cHECK IF IS PUT INTO  ARRAY
-                Console.Write(ResultArray.Count());
+
+            }
+            //cHECK IF IS PUT INTO  ARRAY
+            Console.Write(ResultArray.Count());
 
         }
 
 
-        }
+    }
 }
