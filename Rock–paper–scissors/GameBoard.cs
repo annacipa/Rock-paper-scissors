d1e0662a004af524dc5adada57400b81f7395a2f
Dorontina Cipa <dorontina.cipa@nais.onmicrosoft.com>
2019-08-22 11:38:42 +0200

    Beautified output to Console and test run to 100 rounds

--- Rock–paper–scissors/GameBoard.cs
+++ Rock–paper–scissors/GameBoard.cs
@@ -13,11 +13,8 @@ namespace Rock_paper_scissors
         const int Rounds = 100;
         private
         const String RockOfPC = "Rock";
-        private string[] Values = {
-   "Rock",
-   "Scissors",
-   "Paper"
-  };
+        private string[] Values = {"Rock","Scissors","Paper"};
+
         public List<Results> ResultArray = new List<Results>();
 
 
@@ -33,27 +30,32 @@ namespace Rock_paper_scissors
             Random random_value_of_opponent = new Random();
 
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < Rounds; i++)
             {
                 //random number for component
                 var index = random_value_of_opponent.Next(0, 3);
                 if (index == 2)
                 {
                     //check the round
-                    Console.WriteLine("Round is {0}", i);
+                    Console.WriteLine("Round is {0}", i+1);
                     //INSERT TO ARRAY RESULTS OF USER-WINNER
-                    ResultArray.Add(new Results(Values[index], i + 1, "user"));
-                    Console.Write("Wins user" + Environment.NewLine);
+                    ResultArray.Add(new Results(Values[index], i + 1, "USER"));
+                    var s = String.Format("Random value Chosen: {0} | Value he playd for Results Array{1} | Winner is  {2} |Index {3}", Values[index], ResultArray[i].what_user_played, ResultArray[i].who_won, i);
+                    Console.Write(s);
+                    Console.WriteLine(Environment.NewLine);
 
 
                 }
                 else if (index == 1)
                 {
                     //check the round
-                    Console.WriteLine("Round is {0}", i);
+                    Console.WriteLine("Round is {0}", i+1);
                     //INSERT TO ARRAY RESULTS OF PC-WINNER 
-                    ResultArray.Add(new Results(Values[index], i + 1, "pc"));
-                    Console.Write("Wins pc" + Environment.NewLine);
+                    ResultArray.Add(new Results(Values[index], i + 1, "PC"));
+                    var s = String.Format("Random value Chosen: {0} | Value he playd for Results Array{1} | Winner is  {2} |Index {3}", Values[index], ResultArray[i].what_user_played, ResultArray[i].who_won, i);
+                    Console.Write(s);
+                    Console.WriteLine(Environment.NewLine);
+
                 }
                 else
                 {
--- Rock–paper–scissors/Results.cs
+++ Rock–paper–scissors/Results.cs
@@ -9,7 +9,7 @@ namespace Rock_paper_scissors
         //game round
         public int round;
         //what user played from the random generator
-        public string what_user_playd;
+        public string what_user_played;
         //and who won finally
         public string who_won;
 
@@ -17,7 +17,7 @@ namespace Rock_paper_scissors
         public Results(string what_he_playd_v, int round_r, string who_won_v)
 
         {
-            this.what_user_playd = what_he_playd_v;
+            this.what_user_played = what_he_playd_v;
             this.round = round_r;
             this.who_won = who_won_v;
 
