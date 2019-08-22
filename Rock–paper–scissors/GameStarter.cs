b695985d3a36389487dd270eac4152b23685ceb3
Dorontina Cipa <dorontina.cipa@nais.onmicrosoft.com>
2019-08-22 13:38:21 +0200

    Inserted interactivity at console

--- Rock–paper–scissors/GameStarter.cs
+++ Rock–paper–scissors/GameStarter.cs
@@ -8,12 +8,39 @@ namespace Rock_paper_scissors
 {
     class GameStarter
     {
+
+       
+
         static void Main(string[] args)
         {
+            //Welcoming message
             Console.WriteLine("Welcome to the game of Rock Paper Scissors");
-            Console.WriteLine(Environment.NewLine);
-            GameBoard newGame = new GameBoard();
-            Console.WriteLine("Thank you for Playing");
+            //Instructions for interactivity
+            Console.WriteLine("Type 1 to play,type anyting else  to exit");
+            //a point to enter to go in loop if player wants to play again
+            Enter:  var line = Console.ReadLine();
+            //convert inputed value from string to int
+            int answer = Convert.ToInt32(line);
+            //check the value entered
+            if (answer == 1)
+                {
+                //if 1 then we go to the playinh round
+
+                    GameBoard newGame = new GameBoard();
+                    Console.WriteLine("Thank you for Playing! Want to Play again?Type 1 to play, Type 2 or any key to exit");
+                //check if user wants to play again 
+                goto Enter;
+                }
+             else
+                {
+                    Console.WriteLine("Game Exited");
+
+                }
+
+                Console.WriteLine(Environment.NewLine);
+
+            } 
+
         }
     }
-}
+
